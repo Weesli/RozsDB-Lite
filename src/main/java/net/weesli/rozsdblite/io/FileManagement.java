acf8dea97b8a7f31f0f01a8362f13f08978584ad
Weesli <sefasam553498@gmail.com>
2026-03-06 19:17:29 +0300

    changed database api system

--- src/main/java/net/weesli/rozsdblite/io/FileManagement.java
+++ src/main/java/net/weesli/rozsdblite/io/FileManagement.java
@@ -5,62 +5,23 @@ import java.util.*;
 
 public class FileManagement {
 
-    private Writer writer;
-    private Reader reader;
-    private DatabaseImpl database;
-
-    private final HashMap<String, LinkedHashMap<String, String>> cache = new HashMap<>();
-
+    private final Writer writer;
+    private final Reader reader;
 
     public FileManagement(DatabaseImpl database) {
-        this.database = database;
         writer = new Writer(database, this);
         reader = new Reader(database, this);
-        try {
-            cache.putAll(reader.read());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void put(String tableName, String id, String data) {
-        try {
-            LinkedHashMap<String, String> map = cache.computeIfAbsent(tableName, k -> new LinkedHashMap<>());
-            map.put(id, data);
-        }catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void delete(String tableName, String id) {
-        try {
-            LinkedHashMap<String, String> map = cache.computeIfAbsent(tableName, k -> new LinkedHashMap<>());
-            map.remove(id);
-        }catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-    public String get(String tableName, String id) {
-        try {
-            return cache.computeIfAbsent(tableName, k -> new LinkedHashMap<>()).get(id);
-        }catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 
     public void save() {
         writer.flush();
     }
 
-    public List<String> getAll(String tableName) {
-        try {
-            return new ArrayList<>(cache.computeIfAbsent(tableName, k -> new LinkedHashMap<>()).values());
-        }catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    public Writer getWriter() {
+        return writer;
     }
 
-    public HashMap<String, LinkedHashMap<String, String>> getCache() {
-        return cache;
+    public Reader getReader() {
+        return reader;
     }
 }
